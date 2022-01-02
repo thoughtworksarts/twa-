be99a3f2f641e821851e0fedaf88a8dbb3f893a0
Andrew McWilliams <andy@jahya.net>
2022-01-02 11:00:19 -0500

    Adds grunt to support concatenation to single file

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      files: 'built.gs'
+    },
+    concat: {
+      options: {
+        separator: "\n\n/* -------------------------------------------------------------------------- */\n\n"
+      },
+      files: {
+        src: ['gsheet-/*.gs', '*.gs'],
+        dest: 'built.gs'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.registerTask('default', ['clean', 'concat']);
+};
